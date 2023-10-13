243ccf273c99e41d6b6ac70c4bbdf4ed05756e45
FilipM79 <filip.markovic.79@gmail.com>
2023-10-13 14:07:01 +0200

    Finished video 15. Creating Constants class and restructuring project by adding packages.

--- app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -1,15 +1,12 @@
-package com.example.foody
+package com.example.foody.ui.fragments.recipes
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import androidx.viewbinding.ViewBinding
-import com.cooltechworks.views.shimmer.ShimmerAdapter
 import com.cooltechworks.views.shimmer.ShimmerRecyclerView
-import com.example.foody.databinding.FragmentRecipesBinding
+import com.example.foody.R
 
 class RecipesFragment : Fragment() {
 
