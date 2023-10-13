2a33455c90d4e2d43e10ccba23f5138c9543dbf1
FilipM79 <filip.markovic.79@gmail.com>
2023-10-13 11:24:07 +0200

    Finished video 12, designing RecyclerView placeholder layout which is added for shimmer effect when loading.

--- app/src/main/java/com/example/foody/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/RecipesFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+import com.example.foody.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
+
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding: FragmentRecipesBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+//        binding.recyclerview.showShimmerAdapter()
+
+        return view
     }
 
 }
