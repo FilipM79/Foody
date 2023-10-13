e4a3a57ab695374c9bc6d412793531d5cc039cff
FilipM79 <filip.markovic.79@gmail.com>
2023-10-13 13:49:28 +0200

    Finished video 14. Generating Model classes for API.

--- app/src/main/java/com/example/foody/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/RecipesFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import com.cooltechworks.views.shimmer.ShimmerAdapter
+import com.cooltechworks.views.shimmer.ShimmerRecyclerView
 import com.example.foody.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
 
-    private var _binding: FragmentRecipesBinding? = null
-    private val binding: FragmentRecipesBinding get() = _binding!!
+//    private var _binding: FragmentRecipesBinding? = null
+//    private val binding: FragmentRecipesBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,7 +23,7 @@ class RecipesFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_recipes, container, false)
 
-//        binding.recyclerview.showShimmerAdapter()
+        view.findViewById<ShimmerRecyclerView>(R.id.recyclerview).showShimmerAdapter()
 
         return view
     }
