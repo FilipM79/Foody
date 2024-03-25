5f31a2f291736964a9116554f89531456feb22bf
FilipM79 <filip.markovic.79@gmail.com>
2024-03-25 18:29:48 +0100

    Images removed from ingredients in RecipeDetailsScreen

--- app/src/main/java/com/example/foody/recipe_details/presentation/ui/RecipeDetailsScreen.kt
+++ app/src/main/java/com/example/foody/recipe_details/presentation/ui/RecipeDetailsScreen.kt
@@ -8,14 +8,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
@@ -23,16 +26,24 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -127,10 +138,7 @@ fun RecipeTitleText(title: String) {
             .padding(top = 8.dp, bottom = 4.dp)
             .background(
                 brush = Brush.verticalGradient(
-                    0.3f to Color.White,
-                    0.8f to Color.Gray,
-                    startY = 0f,
-                    endY = 80.0f
+                    0.3f to Color.White, 0.8f to Color.Gray, startY = 0f, endY = 80.0f
                 )
             )
     )
@@ -189,82 +197,110 @@ private fun Ingredients(ingredients: List<Ingredient>) {
             style = MaterialTheme.typography.titleMedium,
             fontWeight = FontWeight.Bold
         )
-        LazyHorizontalGrid(
-            contentPadding = PaddingValues(0.dp),
-            rows = GridCells.Adaptive(150.dp),
+        LazyVerticalGrid(
+            contentPadding = PaddingValues(4.dp),
+            columns = GridCells.Adaptive(150.dp),
             verticalArrangement = Arrangement.spacedBy(3.dp),
             horizontalArrangement = Arrangement.spacedBy(3.dp),
-            modifier = Modifier
-                .height(430.dp),
+            modifier = Modifier.height( ((ingredients.size/2)*80) .dp),
+            userScrollEnabled = true,
             content = {
                 items(
                     count = ingredients.size,
-                    key = null // ???
+                    key = null
                 ) {
-                    Box(modifier = Modifier.padding(horizontal = 8.dp)) {
-                        Card(elevation = CardDefaults.elevatedCardElevation(8.dp)) {
-                            AsyncImage(
-                                model = "${Constants.BASE_URL}/images/ingredients/${ingredients[it].title}-Small.png",
-                                contentDescription = null,
-                                contentScale = ContentScale.FillWidth,
-                                modifier = Modifier
-                                    .size(200.dp)
-                                    .clip(shape = RoundedCornerShape(16.dp))
-                                    .fillMaxWidth()
-                                    .border(
-                                        border = BorderStroke(2.dp, Color.Gray),
-                                        shape = RoundedCornerShape(16.dp)
-                                    )
-                                    .padding(16.dp)
-
+                    Box(modifier = Modifier
+                        .padding(4.dp)
+                        .border(border = BorderStroke(2.dp, Color.Gray), shape = RectangleShape)
+                        .background(MaterialTheme.colorScheme.inverseOnSurface)
+                        .padding(8.dp),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        Column {
+                            Text(
+                                text = ingredients[it].title,
+                                softWrap = true,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1,
+                                style = MaterialTheme.typography.titleMedium,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier.padding(horizontal = 8.dp)
+                            )
+                            Spacer(modifier = Modifier.height(4.dp))
+                            Text(
+                                text = ingredients[it].measure,
+                                softWrap = true,
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1,
+                                textAlign = TextAlign.Center,
+                                fontStyle = FontStyle.Italic,
+                                style = MaterialTheme.typography.titleMedium,
+                                fontWeight = FontWeight.Normal,
+                                modifier = Modifier.padding(horizontal = 8.dp)
                             )
                         }
-                        Box(modifier = Modifier
-                            .padding(8.dp)
-                            .fillMaxWidth()
-                        ) {
-                            Column(modifier = Modifier.fillMaxWidth()) {
-                                Text(
-                                    text = ingredients[it].title,
-                                    textAlign = TextAlign.Center,
-                                    style = MaterialTheme.typography.titleMedium,
-                                    fontWeight = FontWeight.Bold,
-                                    modifier = Modifier
-                                        .clip(shape = RoundedCornerShape(12.dp))
-                                        .fillMaxWidth()
-                                        .background(
-                                            brush = Brush.verticalGradient(
-                                                0f to Color.White,
-                                                1f to Color.Gray,
-                                                startY = 20f,
-                                                endY = 80.0f
-                                            ), alpha = 0.7f
-                                        )
-                                        .padding(4.dp),
-                                    color = Color.Black
-                                )
-                                Spacer(modifier = Modifier.size(8.dp))
-                                Text(
-                                    text = ingredients[it].measure,
-                                    textAlign = TextAlign.Center,
-                                    style = MaterialTheme.typography.titleMedium,
-                                    fontWeight = FontWeight.Bold,
-                                    modifier = Modifier
-                                        .clip(shape = RoundedCornerShape(12.dp))
-                                        .fillMaxWidth()
-                                        .background(
-                                            brush = Brush.verticalGradient(
-                                                0f to Color.White,
-                                                1f to Color.Gray,
-                                                startY = 20f,
-                                                endY = 80.0f
-                                            ), alpha = 0.7f
-                                        )
-                                        .padding(4.dp),
-                                    color = Color.Black
-                                )
-                            }
-                        }
+                        
+//                         For ingredients with images
+//                        Card(elevation = CardDefaults.elevatedCardElevation(8.dp)) {
+//                            AsyncImage(
+//                                model = "${Constants.BASE_URL}/images/ingredients/${ingredients[it].title}-Small.png",
+//                                contentDescription = null,
+//                                contentScale = ContentScale.FillWidth,
+//                                filterQuality = FilterQuality.Low,
+//                                modifier = Modifier.size(200.dp)
+//                                    .clip(shape = RoundedCornerShape(16.dp))
+//                                    .fillMaxWidth()
+//                                    .border(
+//                                        border = BorderStroke(2.dp, Color.Gray),
+//                                        shape = RoundedCornerShape(16.dp)
+//                                    )
+//                                    .padding(12.dp)
+//                            )
+//                        }
+//                        Column(
+//                            modifier = Modifier.fillMaxSize().alpha(0.5f),
+//                            horizontalAlignment = Alignment.CenterHorizontally,
+//                            verticalArrangement = Arrangement.spacedBy(140.dp)
+//                        ) {
+//                            Text(
+//                                text = ingredients[it].title,
+//                                textAlign = TextAlign.Center,
+//                                style = MaterialTheme.typography.titleMedium,
+//                                fontWeight = FontWeight.Bold,
+//                                modifier = Modifier
+//                                    .padding(horizontal = 8.dp)
+//                                    .clip(shape = RoundedCornerShape(12.dp))
+//                                    .fillMaxWidth()
+//                                    .background(
+//                                        brush = Brush.verticalGradient(
+//                                            0f to Color.White,
+//                                            1f to Color.Gray,
+//                                            startY = 20f,
+//                                            endY = 80.0f
+//                                        ), alpha = 0.7f
+//                                    ),
+//                                color = Color.Black
+//                            )
+//                            Text(
+//                                text = ingredients[it].measure,
+//                                textAlign = TextAlign.Center,
+//                                style = MaterialTheme.typography.titleMedium,
+//                                fontWeight = FontWeight.Bold,
+//                                modifier = Modifier
+//                                    .padding(horizontal = 8.dp)
+//                                    .clip(shape = RoundedCornerShape(12.dp))
+//                                    .fillMaxWidth()
+//                                    .background(
+//                                        brush = Brush.verticalGradient(
+//                                            0f to Color.White,
+//                                            1f to Color.Gray,
+//                                            startY = 20f,
+//                                            endY = 80.0f
+//                                        ), alpha = 0.7f
+//                                    ),
+//                                color = Color.Black
+//                            )
+//                        }
                     }
                 }
             }
